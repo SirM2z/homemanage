c5e0bba217bae7dc8e360832c88193d0f5500d9e
z <SirM2z@users.noreply.github.com>
2017-01-11 10:54:35 +0800

    delete

--- src/common.js
+++ src/common.js
@@ -1,13 +1,11 @@
 export const base_url = (function () {
     var url = '';
-    // url="http://api.pstone.cc:8080/bluetoothlock";
-    url = "http://lockmgmt.pstone.cc:8080/lockmgmt";
-    // url = "http://121.41.50.22/:8080/lockmgmt";
+    url = "http://ip:port/lockmgmt";
     return url;
 }());
 
 export const img_url = (function () {
     var url = '';
-    url = "http://lockmgmt.pstone.cc:8080/img/";
+    url = "http://ip:port/img/";
     return url;
 }());
